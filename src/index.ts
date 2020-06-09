e048abf58f7afde195b1b29542f1c49e872136fb
Kazumi Inada <hello@nandenjin.com>
2020-06-09 11:22:49 +0900

    [Add] Renderer, Stats monitor, GUI

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,31 @@
 import './style/index.scss'
+import Stats from 'stats.js'
+import { GUI } from 'dat.gui'
+import { WebGLRenderer } from 'three'
 
-document.body.appendChild(
-  document.createTextNode('Ready. You can start from src/index.ts.')
-)
+const renderer = new WebGLRenderer({ antialias: true })
+document.body.appendChild(renderer.domElement)
+
+const stats = new Stats()
+document.body.appendChild(stats.dom)
+
+const gui = new GUI({ autoPlace: false })
+gui.domElement.classList.add('gui')
+document.body.appendChild(gui.domElement)
+
+function render() {
+  requestAnimationFrame(render)
+  stats.begin()
+  stats.end()
+}
+
+window.addEventListener('load', () => requestAnimationFrame(render))
+
+function onWindowResize() {
+  const w = window.innerWidth,
+    h = window.innerHeight
+  renderer.setSize(w, h)
+}
+
+onWindowResize()
+window.addEventListener('resize', onWindowResize)
